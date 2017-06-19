e4f7d184f77debae2b1522376c3eb3922554c133
Sahil Gulati <sahil@getamplify.com>
2017-06-19 09:45:26 +0530

    Added constants for variables of class exception

---
+++ src/PHPHump/Constants/Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PHPHump\Constants;
+/**
+ * @author Sahil Gulati <sahil.gulati1991@outlook.com>
+ */
+class Exception
+{
+    const FILE="file";
+    const VARIABLE="variable";
+    const ATTRIBUTE="attribute";
+    const VALIDATOR="validator";
+}
