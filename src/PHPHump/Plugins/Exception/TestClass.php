1dd2e907782fd9494589fd8fb99addc1420a6853
Sahil Gulati <sahil@getamplify.com>
2017-07-24 12:43:51 +0530

    Added a sample plugin class for more understanding

---
+++ src/PHPHump/Plugins/Exception/TestClass.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @note This plugin class will not work, because it is not defined.
+ * Plugin class and file name could be 
+ * \PHPHump\Plugins\Exception\File
+   \PHPHump\Plugins\Exception\Variable
+   \PHPHump\Plugins\Exception\Attribute
+   \PHPHump\Plugins\Exception\Validator
+ * @author Sahil Gulati <sahil.gulati1991@outlook.com>
+ * @desc
+ */
+namespace \PHPHump\Plugins\Exception;
+class TestClass
+{
+    public static function getMessage()
+    {
+        /**
+         * There you will receive few parameters which can be used for further processing
+         */
+    }
+    public static function getExtendedMessage()
+    {
+        /**
+         * There you will receive few parameters which can be used for further processing
+         */
+    }
+}
