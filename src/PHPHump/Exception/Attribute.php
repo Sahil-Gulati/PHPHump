2e99e481d05fb818d2db55fc2a265f0724e04104
Sahil Gulati <sahil@getamplify.com>
2017-07-25 16:35:24 +0530

    Added new function for handling loop array variable exception

--- src/PHPHump/Exception/Attribute.php
+++ src/PHPHump/Exception/Attribute.php
@@ -14,7 +14,8 @@ class Attribute
         2 => 'getWhileCounterError',
         3 => 'getWhileVariableError',
         4 => 'getAssignError',
-        5 => 'getRequireError'
+        5 => 'getRequireError',
+        6 => 'getLoopVariableError',
     );
 
 
@@ -69,6 +70,16 @@ class Attribute
         $string.="<p style='color:#6d5c5c;font-family:monospace;font-size:12px;margin-bottom:1px;margin-top:5px;font-weight:bolder'>A valid <span style=".self::$configVarStype.">Hump-Loop</span> syntax can have two formats:</p>";
         $string.="<p style='color:#6d5c5c;font-family:monospace;font-size:12px;margin-bottom:1px;margin-top:5px;font-weight:bolder'><u>1</u>. <i>variableName as valueVariableName<i></p>";
         $string.="<p style='color:#6d5c5c;font-family:monospace;font-size:12px;margin-bottom:1px;margin-top:5px;font-weight:bolder'><u>2</u>. <i>variableName as keyVariableName -> valueVariableName<i></p>";
+        $string.="<p style='color:#6d5c5c;font-family:monospace;font-size:12px;margin-bottom:1px;margin-top:5px;font-weight:bolder'></p>";
+        return $string;
+    }
+    private static function getLoopVariableError()
+    {
+        $variableName=func_get_args()[1];
+        $string="<h1 style='color:#c44b4b;font-size:20px;margin-bottom:5px;'>PHPHump Exception!</h1>";
+        $string.="<p style='color:#6d5c5c;font-family:monospace;font-size:16px;margin-bottom:1px;font-weight:bolder'>Invalid loop variable!</p>";
+        $string.="<p style='color:#6d5c5c;font-family:monospace;font-size:12px;margin-bottom:8px;margin-top:1px;font-weight:bolder'>Loop variable should be an array.</p>";
+        $string.="<p style='color:#6d5c5c;font-family:monospace;font-size:12px;margin-bottom:1px;margin-top:1px;font-weight:bolder'>You can't prevent Hump syntax validation error by config.</p>";
         $string.="<p style='color:#6d5c5c;font-family:monospace;font-size:12px;margin-bottom:1px;margin-top:5px;font-weight:bolder'></p>";
         return $string;
     }
